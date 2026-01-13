74e84da702cd837755a71df617fbeba75ea897ed
KISHAN1506 <kishanagarwal1506@gmail.com>
2026-01-13 23:56:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (46.85%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int hammingWeight(int num) {
+        int count = 0;
+        while(num){
+            num = (num & num-1);
+            count++;
+        }
+        return count;
+    }
+};
