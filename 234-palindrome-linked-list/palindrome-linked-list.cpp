f0b9de55534485e9cee12c5d57f059514c9a3bc8
Kishan Agarwal <kishanagarwal1506@gmail.com>
2026-02-08 23:47:42 +0530

    Time: 7 ms (29.53%) | Memory: 121.8 MB (46.85%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -10,28 +10,69 @@
  */
 class Solution {
 public:
+    ListNode* reverseLL(ListNode* head){
+        ListNode* temp = head;
+        ListNode* prev = NULL;
+        ListNode* front = NULL;
+        while(temp != nullptr){
+            front = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev;
+    }
+
     bool isPalindrome(ListNode* head) {
 
-        // Memory can be sorted using Stack :
-        stack<int> st;
-        ListNode* mover = head;
-        while(mover != nullptr){
-            st.push(mover->val);
-            mover = mover->next;
-        }
-
-        ListNode* mover2 = head;
-        while(mover2 != nullptr){
-            if(st.top() == mover2->val){
-                st.pop();
-            }
-            mover2 = mover2->next;
-        }
-
-        if(st.empty()){
+        if(head == nullptr || head->next == nullptr){
             return true;
         }
-        return false;
+        // Optimization for space -> O(1);
+        ListNode* fast = head;
+        ListNode* slow = head;
+
+        while(fast->next != nullptr && fast->next->next != nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* newHead = reverseLL(slow->next);
+
+        ListNode* first = head;
+        ListNode* second = newHead;
+        while(second != NULL){
+            if(first->val != second->val){
+                reverseLL(newHead);
+                return false;
+            }
+            first = first->next;
+            second = second->next;
+        }
+        reverseLL(newHead);
+        return true;
+
+
+        // Memory can be sorted using Stack :, but still solution is not taking O(1) sc
+        // stack<int> st;
+        // ListNode* mover = head;
+        // while(mover != nullptr){
+        //     st.push(mover->val);
+        //     mover = mover->next;
+        // }
+
+        // ListNode* mover2 = head;
+        // while(mover2 != nullptr){
+        //     if(st.top() == mover2->val){
+        //         st.pop();
+        //     }
+        //     mover2 = mover2->next;
+        // }
+
+        // if(st.empty()){
+        //     return true;
+        // }
+        // return false;
 
 
 
