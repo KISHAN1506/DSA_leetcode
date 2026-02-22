83c0012e87a23af3e040c6944a2c3ed3e3e87786
Kishan Agarwal <kishanagarwal1506@gmail.com>
2026-02-22 11:39:05 +0530

    Time: 4 ms (22.03%) | Memory: 23.8 MB (19.13%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -11,23 +11,42 @@
  */
 class Solution {
 public:
-    int heightOfTree(TreeNode* root){
-        if(root == NULL) return 0;
-        int height = max(heightOfTree(root->right),heightOfTree(root->left)) + 1;
-        return height;
-    }
-
-    int diaOfTree(TreeNode* root){
-        if(root == NULL) return 0;
-        int currDiam = heightOfTree(root->left)+heightOfTree(root->right);
-        int leftDiam = diaOfTree(root->left);
-        int rightDiam = diaOfTree(root->right);
-
-        return max(currDiam,max(leftDiam,rightDiam));
-        
+    // Optimal Approach: --> O(n) calculating height and diameter simultaneously by using LeftHeight and rightHeight
+    int height(TreeNode* root,int &diameter){
+        if(root == NULL){
+            return 0;
+        }
+        int lh = height(root->left,diameter);
+        int rh = height(root->right,diameter);
+        diameter = max(diameter,lh+rh); // this is finding and updating diameter accordingly in every call
+        return 1+max(lh,rh); // this is calculating height of the tree
     }
 
     int diameterOfBinaryTree(TreeNode* root) {
-        return diaOfTree(root);
+        int diameter = 0;
+        height(root,diameter);
+        return diameter;
     }
+
+
+    // Naive Approach --> O(n^2)
+    // int heightOfTree(TreeNode* root){
+    //     if(root == NULL) return 0;
+    //     int height = max(heightOfTree(root->right),heightOfTree(root->left)) + 1;
+    //     return height;
+    // }
+
+    // int diaOfTree(TreeNode* root){
+    //     if(root == NULL) return 0;
+    //     int currDiam = heightOfTree(root->left)+heightOfTree(root->right);
+    //     int leftDiam = diaOfTree(root->left);
+    //     int rightDiam = diaOfTree(root->right);
+
+    //     return max(currDiam,max(leftDiam,rightDiam));
+        
+    // }
+
+    // int diameterOfBinaryTree(TreeNode* root) {
+    //     return diaOfTree(root);
+    // }
 };
