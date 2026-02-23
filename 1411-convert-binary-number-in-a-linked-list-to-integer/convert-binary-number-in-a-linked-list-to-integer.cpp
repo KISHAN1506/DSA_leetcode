272ae98f38afa90fe6a8164772d6a11ec74a9878
Kishan Agarwal <kishanagarwal1506@gmail.com>
2026-02-23 20:47:20 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (39.71%) - LeetSync

--- 1411-convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.cpp
+++ 1411-convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -10,34 +10,47 @@
  */
 class Solution {
 public:
-    int powFix(int a,int n){
-        if(n == 0){
-            return 1;
-        }
-
-        if(n%2 == 1){
-            return a*powFix(a,n-1);
-        }else{
-            int val = powFix(a,n/2);
-            return val*val;
-        }
-    }
-
+    // Optimal O(n)
     int getDecimalValue(ListNode* head) {
-        ListNode* mover = head;
-        vector<int> vals;
-        while(mover != NULL){
-            vals.push_back(mover->val);
-            mover = mover->next;
-        }
-        int n = 0;
         int ans = 0;
-
-        for(int i = vals.size()-1;i>=0;i--){
-            int digit = vals[i];
-            if(digit != 0) ans += powFix(2,n)*digit;
-            n++;
+        
+        while (head != NULL) {
+            ans = ans * 2 + head->val;
+            head = head->next;
         }
+        
         return ans;
     }
+    
+    // int powFix(int a,int n){
+    //     if(n == 0){
+    //         return 1;
+    //     }
+
+    //     if(n%2 == 1){
+    //         return a*powFix(a,n-1);
+    //     }else{
+    //         int val = powFix(a,n/2);
+    //         return val*val;
+    //     }
+    // }
+
+    // int getDecimalValue(ListNode* head) {
+    //     ListNode* mover = head;
+    //     vector<int> vals;
+    //     while(mover != NULL){
+    //         vals.push_back(mover->val);
+    //         mover = mover->next;
+    //     }
+    //     int n = 0;
+    //     int ans = 0;
+
+    //     for(int i = vals.size()-1;i>=0;i--){
+    //         int digit = vals[i];
+    //         if(digit != 0) ans += powFix(2,n)*digit;
+    //         n++;
+    //     }
+    //     return ans;
+    // }
+
 };
