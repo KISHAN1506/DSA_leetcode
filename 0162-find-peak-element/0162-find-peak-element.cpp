c387175c74420176dd1195c400a4caac6d22ffa3
Kishan Agarwal <kishanagarwal1506@gmail.com>
2026-06-25 01:47:05 +0530

    Time: 0 ms (100%), Space: 12.7 MB (39.24%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -5,17 +5,39 @@ public:
         if(n == 1){
             return 0;
         }
-        if(n == 2){
-            if(arr[0] > arr[1]){
-                return 0;
+        if(arr[0] > arr[1]) return 0;
+        if(arr[n-1] > arr[n-2]) return n-1;
+        int low = 1;
+        int high = n-2;
+
+        while(low <= high){
+            int mid = low + ((high - low)/2);
+
+            if(arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1]){
+                return mid;
+            }else if(arr[mid] > arr[mid-1]){
+                low = mid+1;
+            }else{
+                high = mid-1;
             }
-            return 1;
         }
-        for(int i = 1;i<n-1;i++){
-            if(arr[i] > arr[i-1] && arr[i] > arr[i+1]){
-                return i;
-            }
-        }
-        return arr[n-1]>arr[0] ? n-1 : 0;
+        return -1;
+        
+        // int n = arr.size();
+        // if(n == 1){
+        //     return 0;
+        // }
+        // if(n == 2){
+        //     if(arr[0] > arr[1]){
+        //         return 0;
+        //     }
+        //     return 1;
+        // }
+        // for(int i = 1;i<n-1;i++){
+        //     if(arr[i] > arr[i-1] && arr[i] > arr[i+1]){
+        //         return i;
+        //     }
+        // }
+        // return arr[n-1]>arr[0] ? n-1 : 0;
     }
 };
