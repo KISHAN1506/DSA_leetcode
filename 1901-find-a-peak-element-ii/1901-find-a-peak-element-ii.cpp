a519c24f66ce1bdc52025fda91f77a54f74db302
Kishan Agarwal <kishanagarwal1506@gmail.com>
2026-07-09 01:20:12 +0530

    Time: 0 ms (100%), Space: 50.1 MB (86.51%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -1,16 +1,52 @@
 class Solution {
 public:
-    vector<int> findPeakGrid(vector<vector<int>>& mat) {
-        int maxVal = mat[0][0];
-        pair<int,int> idx = {0,0};
-        for(int i = 0;i<mat.size();i++){
-            for(int j = 0;j<mat[0].size();j++){
-                if(mat[i][j] > maxVal){
-                    maxVal = mat[i][j];
-                    idx = {i,j};
-                }
+    int findMaxIdx(vector<vector<int>>& mat,int n,int m,int col){
+        int maxValue = -1;
+        int index = -1;
+
+        for(int i = 0;i<n;i++){
+            if(mat[i][col] > maxValue){
+                maxValue = mat[i][col];
+                index = i;
             }
         }
-        return {idx.first,idx.second};
+
+        return index;
+    }
+
+    vector<int> findPeakGrid(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        int low = 0;
+        int high = m-1;
+
+        while(low <= high){
+            int mid = low + ((high - low)/2);
+            int maxRowIdx = findMaxIdx(mat,n,m,mid);
+
+            int left = mid-1 >=0 ? mat[maxRowIdx][mid-1] : -1;
+            int right = mid+1 <m ? mat[maxRowIdx][mid+1] : -1;
+
+            if(mat[maxRowIdx][mid]> left && mat[maxRowIdx][mid] > right){
+                return {maxRowIdx,mid};
+            }else if(mat[maxRowIdx][mid] < left){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return {-1,-1};
+
+        // int maxVal = mat[0][0];
+        // pair<int,int> idx = {0,0};
+        // for(int i = 0;i<mat.size();i++){
+        //     for(int j = 0;j<mat[0].size();j++){
+        //         if(mat[i][j] > maxVal){
+        //             maxVal = mat[i][j];
+        //             idx = {i,j};
+        //         }
+        //     }
+        // }
+        // return {idx.first,idx.second};
     }
 };
