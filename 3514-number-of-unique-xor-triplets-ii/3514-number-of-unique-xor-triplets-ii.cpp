f1bc70cb8919e6997541fe7989d208f78efca6b4
Kishan Agarwal <kishanagarwal1506@gmail.com>
2026-07-24 18:17:15 +0530

    Time: 696 ms (62.92%), Space: 35.1 MB (83.15%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
@@ -2,21 +2,52 @@ class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
         int n = nums.size();
+        int maxEle = *max_element(nums.begin(),nums.end());
 
-        unordered_set<int> s1;
+        int T = 1;
+        while(T <= maxEle){
+            T *= 2;
+        }
+
+        vector<bool> s1(T,false); //to store pairXOR value
+        vector<bool> s2(T,false); //to store triplet XOR value
+
         for(int i = 0;i<n;i++){
-            for(int j = i;j<n;j++){
-                s1.insert(nums[i]^nums[j]);
+            for(int j  = 0;j<n;j++){
+                s1[nums[i]^nums[j]] = true;
             }
         }
 
-        unordered_set<int> stt;
-        for(int pairXor:s1){
-            for(int& num:nums){
-                stt.insert(pairXor^num);
+        for(int i = 0;i<T;i++){
+            if(s1[i] == true){
+                for(int& num : nums){
+                    s2[i^num] = true;
+                }
             }
         }
 
-        return stt.size();
+        int cnt = 0;
+        for(int i = 0;i<T;i++){
+            if(s2[i] == true) cnt++;
+        }
+        return cnt;
+
+
+        // int n = nums.size();
+        // unordered_set<int> s1;
+        // for(int i = 0;i<n;i++){
+        //     for(int j = i;j<n;j++){
+        //         s1.insert(nums[i]^nums[j]);
+        //     }
+        // }
+
+        // unordered_set<int> stt;
+        // for(int pairXor:s1){
+        //     for(int& num:nums){
+        //         stt.insert(pairXor^num);
+        //     }
+        // }
+
+        // return stt.size();
     }
 };
