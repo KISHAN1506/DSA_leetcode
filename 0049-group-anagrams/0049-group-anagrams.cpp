48bdc87c75d412f85ed8ca110d0af373fd6e89fd
Kishan Agarwal <kishanagarwal1506@gmail.com>
2026-08-22 12:42:45 +0530

    Time: 15 ms (69.46%), Space: 28.6 MB (19.35%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,29 +2,33 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         int n = strs.size();
+        unordered_map<string, vector<string>> mpp;
         vector<vector<string>> result;
 
-        unordered_map<string, vector<string>> mpp;
-
         for (int i = 0; i < n; i++) {
-            string temp = strs[i];
-            sort(temp.begin(), temp.end());
+            vector<int> arrTable(26, 0);
+            for (auto& it : strs[i]) {
+                arrTable[it-'a']++;
+            }
 
-            mpp[temp].push_back(strs[i]);
+            // this arrayTable has been populated with the values;
+            // now we can make a new string by just iterating on the arrTable
+
+            string strNew = "";
+            for (int i = 0; i < 26; i++) {
+                while (arrTable[i] != 0) {
+                    strNew += i +'a';
+                    arrTable[i]--;
+                }
+            }
+
+            mpp[strNew].push_back(strs[i]);
         }
 
-        // for (auto& it : mpp) {
-        //     cout << "Key: " << it.first << endl;
-        //     for (auto& str : it.second) {
-        //         cout << str << " ";
-        //     }
-        //     cout << endl;
-        // }
-        
-
-        for(auto& it :mpp){
+        for (auto& it : mpp) {
             result.push_back(it.second);
         }
+
         return result;
     }
 };
