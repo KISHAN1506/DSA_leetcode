f567832d907718864c7370b2a07e7fbf6a8ab088
Kishan Agarwal <kishanagarwal1506@gmail.com>
2026-08-31 23:10:48 +0530

    Time: 0 ms (100%), Space: 15 MB (87.27%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -16,28 +16,43 @@ public:
 
 class Solution {
 public:
+    // 1. insert the copy nodes in between 
+    // 2. Connect the random pointers
+    // 3. Connect the next pointers
+    // 4. make all the 2 linked list free from each other
     Node* copyRandomList(Node* head) {
-        if(head == NULL) return NULL;
-
+        // 1. insert the copy nodes in between 
         Node* temp = head;
-        unordered_map<Node*,Node*> mpp;
-
-        while(temp != NULL){
+        while(temp != nullptr){
             Node* newNode = new Node(temp->val);
-            mpp[temp] = newNode;
+            newNode->next = temp->next ? temp->next : NULL;
+            temp->next = newNode;
 
-            temp = temp->next;
+            temp = temp->next->next;
         }
 
+        // 2. Connect the random pointers
+        // idhar random pointers connect hogya
+        temp = head;
+
+        while(temp != nullptr){
+            Node* copyNode = temp->next;
+            copyNode->random = temp->random ? temp->random->next : NULL;
+            temp = temp->next->next;
+        }
+
+        // 3. Connect the next pointers -> next baadme hi connect hoga while removing connections
+        Node* dummyNode = new Node(-1);
+        Node* res = dummyNode;
         temp = head;
 
         while(temp != NULL){
-            Node* copyNode = mpp[temp];
-            copyNode->next = mpp[temp->next];
-            copyNode->random = temp->random ? mpp[temp->random] : NULL;
+            res->next = temp->next;
+
+            res = res->next;
+            temp->next = temp->next->next;
             temp = temp->next;
         }
-
-        return mpp[head];
+        return dummyNode->next;
     }
 };
